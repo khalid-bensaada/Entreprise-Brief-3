6b7ba26dc74e0664a7d3f043073d7efd9e4d55f9
khalid-bensaada <kbensaada953@gmail.com>
2025-11-19 15:00:50 +0100

    added the part of photo for user in html and tialwind

---
+++ main.js
@@ -0,0 +1,5 @@
+let btnOne = document.getElementById("btnOne");
+let btnTwo = document.getElementById("btnTwo");
+let btnTree = document.getElementById("btnTree");
+let forma = document.getElementById("forma");
+let exper = document.getElementById("exper");
