df7baf0a6facecc7830f9d4b652f6a8d1cd367b9
khalid-bensaada <kbensaada953@gmail.com>
2025-11-20 11:36:42 +0100

    added part of save informations of perssones in js

--- main.js
+++ main.js
@@ -13,10 +13,36 @@ let roomFive = document.getElementById("roomFive");
 let roomsx = document.getElementById("roomsx");
 let fullName = document.getElementById("fullName");
 let email = document.getElementById("email");
-let Photo = document.getElementById("Photo"); 
+let Photo = document.getElementById("Photo");
 let phone = document.getElementById("phone");
 let roles = document.getElementById("roles");
 let dateOne = document.getElementById("dateOne");
 let dateTwo = document.getElementById("dateTwo");
 let company = document.getElementById("company");
+
+
+let formations = [];
+
+btnOne.addEventListener('click' , function(){
+    forma.classList.toggle("hidden")
+});
+
+
+function saveforamtions(){
+    
+    user={
+        fullname : inputName.value ,
+        email : inputEmail.value,
+        photo : inputPhoto.value,
+        phone : inputphone.value,
+        roles : choiceRoles.value,
+        dateone : inputDateOne.value,
+        datetwo : inputdateTwo.value,
+        company : inputcompany.value
+
+    };
+
+    localStorage.setItem("userData", JSON.stringify(user));
+    alert("Data saved successfully!");
+}
 
